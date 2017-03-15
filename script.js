bc583b8a8cced2f4ba6da00b0657dcfc5490a620
Venzel13 <Venzel13@gmail.com>
2017-03-15 12:25:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,126 +1,103 @@
-   (function() {
-   var xhr = new XMLHttpRequest();
+  (function() {
+    var xhr = new XMLHttpRequest();
 
-   xhr.open('GET', 'questions.json', true);
+    xhr.open('GET', 'questions.json', true);
 
-   xhr.send();
+    xhr.send();
 
-   xhr.onreadystatechange = function() {
-     if (xhr.readyState != 4) return;
-     var questions = JSON.parse(xhr.responseText);
-     
-     tmplCommonContainer = _.template(createCommonContainer());
-     document.getElementById('template').innerHTML = tmplCommonContainer({
-       list: questions
-     });
-    
-   }
- })();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4) return;
+      var questions = JSON.parse(xhr.responseText);
 
- function createCommonContainer() {
-   return `<% for(let i=0 ; i < list.length; i++) { %>
-             <div class='container'>
-               <p><%-list[i].header%></p>
-               
-                <%if(list[i].tip) { %>
-                  <div> <%-list[i].question%><span class='toggleTip'>?</span></div>
-                  <div class='tip' hidden><%-list[i].tip%></div>
-                <% } else { %>
-                  <div><%-list[i].question%></div>
-                <%}%>
-                
-                <%switch(list[i].type) { case 'single': %>
-                <div><%=createSingle(list[i].body)%></div>
-                <% break; case 'multiple': %>
-                <div><%=createMultiple(list[i].body)%></div>
-                <% break; default: %>
-                <div><%=createVoluntary()%></div>
-              <%}%>
-
-             </div>
-           <%}%>`
- }
- 
+      createQuestionContainer(questions);
+    };
+  })();
 
 
- function createVoluntary() {
-   return '<input type="textarea">'
- }
- 
- function createMultiple(answersArray) {
-   let resultArray = answersArray.map(function(answer) {
-     if(answer.other) {
-       return "<label><input type='checkbox' onclick='a(this)' class='notNone'>" + answer.text + "<input class='clarification' hidden></label><br>"
-     }
-     
-     if(!answer.none) {
-       return "<label><input type='checkbox' class='notNone'>" + answer.text + "</label><br>";
-     } else {
-       return "<label><input type='checkbox' onclick='disableAnswers(this)'>" + answer.text + "</label><br>";
-     }
-   });
-  return resultArray.join("")
- }
- 
- function createSingle(answersArray, i) {
-   let resultArray = answersArray.map(function(answer) {
-     if(answer.other) {
-       return "<label><input type='radio' onmousedown='this.isChecked = this.checked' name=" + i + " onclick='a(this)' class='notNone'>" + answer.text + "<input class='clarification' hidden></label><br>"
-     }
-     
-     if(!answer.none) {
-       return "<label><input type='radio' onmousedown='this.isChecked = this.checked' onclick='this.checked = !this.isChecked' name=" + i + " class='notNone'>" + answer.text + "</label><br>";
-     } else {
-       return "<label><input type='radio' onmousedown='this.isChecked = this.checked' name=" + i + " onclick='disableAnswers(this)'>" + answer.text + "</label><br>";
-     }
-   });
-  return resultArray.join("")
- }
- 
- function disableAnswers(elem) {
-   if(elem.type != 'checkbox') {
-   elem.checked = !elem.isChecked;
-   }
-   let notNone = document.getElementsByClassName('notNone');
-   for(let i = 0; i < notNone.length; i++) {
-     notNone[i].disabled = !notNone[i].disabled
-   }
-   let clarification = document.querySelector('.clarification');
-   clarification.disabled = !clarification.disabled;
- }
+  function createQuestionContainer(options) { // options объект аргументов
+  
+    for (var i = 0; i < options.length; i++) {
+      var container = createContainer("div", document.body, null, "container");
+      createContainer("p", container, options[i].header);
+      var question = createContainer("div", container, options[i].question);
 
- function toggleClarification(elem) {
-   if(elem.type != 'checkbox') {
-   elem.checked = !elem.isChecked
-   }
-   
-   
-   let clarification = document.querySelector('.clarification');
-   this.hidden = !this.hidden;
- }
- 
-  var a = toggleClarification.bind(clarification)
- 
- 
- 
+      if (options[i].tip) { // для вопросов с подсказкой
+        var questionMark = createContainer("span", question, "?", "toggleTip");
+        let tip = createContainer("div", container, options[i].tip, "tip"); // let решает проблему с замыканием (свой tip при каждой итерации), т.к в var tip попадает только последняя итерация i
+        tip.hidden = true;
 
- document.onclick = function(event) {
-     if (event.target.className == 'toggleTip') {
-       openTip(event);
-     } else {
-       closeTip(event);
-     }
+        // обработчик при клике на вопрос
+        questionMark.onclick = function(event) {
+          event.stopPropagation(); // событие не должно всплывать для скрытия по документу
+          tip.hidden = false;
+        };
+      }
+      // ответы для разных типов вопросов
+      switch (options[i].type) {
+        case "single": // radio-button
+          createAnswer(container, options[i].body, "radio", i);
+          break;
+        case "multiple": // checkbox
+          createAnswer(container, options[i].body, "checkbox");
+          break;
+        default: // textarea
+          createContainer("input", container);
+          break;
+      }
+    }
+
+    // отслеживаем клик на документе
+    document.body.onclick = function() {
+      var tips = document.getElementsByClassName("tip");
+      for (var i = 0; i < tips.length; i++) {
+        tips[i].hidden = true;
+      }
+    };
   }
   
-  function openTip(event) {
-    event.target.closest('.container').querySelector('.tip').hidden = false;
+  function createAnswer(parent, answers, type, name) {
+    var notNone = [];
+    var clarification = createContainer("input", document.body);
+    answers.forEach(answer => {
+      var label = createContainer("label", parent);
+      var elem = createContainer("input", label);
+      elem.type = type;
+      elem.name = name;
+      createContainer("span", label, answer.text);
+
+      if(!answer.none) {
+        notNone.push(elem);
+      }
+      
+      if(answer.none) {
+        elem.onclick = function() {
+          notNone.forEach(function(notNoneElem) {
+            notNoneElem.disabled = !notNoneElem.disabled;
+          });
+        }
+      }
+      
+      if(answer.other) {
+        elem.onclick = function() {
+          clarification.hidden = !clarification.hidden;
+          
+          
+        }
+      }
+      
+    });
   }
 
-  function closeTip(event) {
-    event.target.closest('.container').querySelector('.tip').hidden = true;
+  function createContainer(type, parent, html, className) {
+    var container = document.createElement(type);
+    if (className) {
+      container.classList.add(className);
+    }
+    if (html) {
+      container.innerHTML = html;
+    }
+    parent.appendChild(container);
+    return container;
   }
   
-  
-
- 
- 
+  // elem.checked = !elem.isChecked;
