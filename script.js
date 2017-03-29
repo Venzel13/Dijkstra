1daae8405f9bd842e78bd44733cf5e460747600e
Venzel13 <Venzel13@gmail.com>
2017-03-29 12:21:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
   (function() {
-    
     var xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'questions.json', true);
-
     xhr.send();
     
     function loadQuestions() {
@@ -12,199 +10,124 @@
       
       createQuestionnaire(questions);
     }
-
-    xhr.onreadystatechange = loadQuestions
-
+    xhr.onreadystatechange = loadQuestions;
   })();
-
-
-  function createQuestionnaire(questions) {
-    var lastTip;
+  
+  function createQuestionnaire(questions) { // функция получается очень большая, хоть она и основная. Ничего? или стоит как то разбить функционал?
     for (var i = 0; i < questions.length; i++) {
-      var container = createElem({type: "div", html: null, className: "container"});
-      document.getElementById('questionnaire').appendChild(container);
-      var header = createElem({type: "p", html: questions[i].header});
-      container.appendChild(header);
-      var question = createElem({type: "div", html: questions[i].question});
-      container.appendChild(question);
+      var container = document.getElementById('questionnaire').appendChild(createContainer());
+      container.appendChild(createHeader(questions[i].header));
+      var question = container.appendChild(createQuestion(questions[i].question));
       
-      if (questions[i].tip) {
-        var {tip, questionMark} = createTip(questions[i].tip);
-        container.appendChild(tip);
-        question.appendChild(questionMark);
-        
-        (function(tip) {
-        questionMark.onclick = function(ev) {
-          ev.stopPropagation();
-          if (lastTip)
-            lastTip.hidden = true;
-          tip.hidden = false;
-          lastTip = tip;
-        };
-        })(tip)
+      if(questions[i].tip) {
+        var tip = container.appendChild(createTip(questions[i].tip));
+        var questionMark = question.appendChild(createQuestionMark());
       }
       
-      switch (questions[i].type) {
+      switch(questions[i].type) {
         case "single":
-          var single = createAnswers({answers: questions[i].body, type: "radio", name: i});
-          container.appendChild(single);
+          var single = new Single(questions[i].body);
+          var elem = single.getElem();
+          container.appendChild(elem);
           break;
         case "multiple":
-          var multiple = createAnswers({answers: questions[i].body, type: "checkbox"});
-          container.appendChild(multiple);
+          var multiple = new Multiple(questions[i].body);
+          var elem = multiple.getElem();
+          container.appendChild(elem);
           break;
+          
         case "voluntary":
-          var voluntary = createElem({type: "input"});
-          container.appendChild(voluntary);
           break;
+          
         case "ranging":
-          var blockOfAnswers = createRangingBlock(questions[i].body);
-          container.appendChild(blockOfAnswers);
           break;
       }
     }
-    
-    document.body.onclick = function() {
-      if (lastTip) {
-        lastTip.hidden = true;
-      }
-    };
-  }
-
-  function createAnswers({answers, type, name}) {
-    var notNone = [];
-    var uncheckedElements = [];
-    var clarification;
-    var elem;
-    
-    var blockOfAnswers = createElem({type: "div"});
-    
-    for (var i = 0; i < answers.length; i++) {
-      var label = createElem({type: "label"});
-      blockOfAnswers.appendChild(label);
-      elem = createElem({type: "input"});
-      label.appendChild(elem)
-      elem.type = type;
-      elem.name = name;
-      
-      uncheckElem(uncheckedElements, elem);
-      
-      var span = createElem({type: "span", html: answers[i].text});
-      label.appendChild(span);
-      
-      if(answers[i].other) {
-        clarification = createElem({type: "input"});
-        label.appendChild(clarification);
-        clarification.hidden = true;
-        elem.onclick = function() {
-          clarification.hidden = !clarification.hidden;
-        }
-      }
-
-      if(!answers[i].none) {
-        notNone.push(elem);
-      } else {
-        elem.onclick = function() {
-          disableAnswers(notNone, clarification)
-        }
-      }
-    }
-    return blockOfAnswers
-    
-  }
-
-  function createElem({type, html, className}) {
-    var element = document.createElement(type);
-    if (className) {
-      element.classList.add(className);
-    }
-    if (html) {
-      element.innerHTML = html;
-    }
-    return element;
   }
   
-  function createRangingBlock(answers) {
-   answers = answers.sort(compareRangingAnswers);
+  function createContainer() {
+    var container = document.createElement('div');
+    container.className = 'container';
     
-    var blockOfAnswers = createElem({type: "div"});
-
-    var availableBlock = createElem({type: "select", html: null, className: "availableBlock"});
-    var selectedBlock = createElem({type: "select", html: null, className: "selectedBlock"});
-    availableBlock.size = answers.length;
-    availableBlock.multiple = true;
-    selectedBlock.size = answers.length;
-    selectedBlock.multiple = true;
-    var ASbutton = createElem({type: "button", html: ">>", className: "ASbutton"});
-    var SAbutton = createElem({type: "button", html: "<<", className: "SAbutton"});
-    
-    var buttons = createElem({type: "div", html: null, className: "buttons"});
-    
-    blockOfAnswers.appendChild(availableBlock);
-    buttons.appendChild(ASbutton);
-    buttons.appendChild(SAbutton);
-    blockOfAnswers.appendChild(buttons);
-    blockOfAnswers.appendChild(selectedBlock);
-    
-    for (var i = 0; i < answers.length; i++) {
-      var option = createElem({type: "option", html: answers[i].text});
-      availableBlock.appendChild(option);
-    }
-    
-    ASbutton.onclick = function() {
-      for (var i = 0; i < availableBlock.options.length; i++) {
-        var option = availableBlock.options[i];
-        option.i = i;
-        if(option.selected) {
-          selectedBlock.appendChild(option);
-        }
-      }
-    }
-    
-    SAbutton.onclick = function() {
-      for (var i = 0; i < selectedBlock.options.length; i++) {
-        var option = selectedBlock.options[i];
-        if(option.selected) {
-          availableBlock.insertBefore(option, availableBlock.options[option.i]);
-        }
-      }
-    }
-    return blockOfAnswers;
+    return container;
   }
   
-  function compareRangingAnswers(a, b) {
-    return a.text.localeCompare(b.text);
+  function createHeader(text) {
+    var header = document.createElement('p');
+    header.innerHTML = text;
+    
+    return header;
   }
   
-  function mousedown() {
-    this.isChecked = this.checked;
-  }
-  function click() {
-    this.checked = !this.isChecked;
-  }
-
-// uncheck radio-button when element is clicked again
-  function uncheckElem(uncheckedElements, elem) {
-    uncheckedElements.push(elem);
+  function createQuestion(text) {
+    var question = document.createElement('div');
+    question.innerHTML = text;
     
-    for(var i = 0; i < uncheckedElements.length; i++) {
-      uncheckedElements[i].addEventListener("mousedown", mousedown.bind(uncheckedElements[i])); // onclick handler won't be rewritten
-      uncheckedElements[i].addEventListener("click", click.bind(uncheckedElements[i]));
-    }
+    return question;
   }
   
   function createTip(text) {
-    var questionMark = createElem({type: "span", html: "?", className: "toggleTip"});
-    var tip = createElem({type: "div", html: text, className: "tip"});
-    tip.hidden = true;
-    return {tip, questionMark};
+    var tip = document.createElement('div');
+    tip.innerHTML = text;
+    tip.className = 'tip';
+    
+    return tip;
   }
   
-// disable not none elements
-  function disableAnswers(notNone, clarification) {
-    for(var i = 0; i < notNone.length; i++) {
-      notNone[i].disabled = !notNone[i].disabled;
-    }
-    clarification.disabled = !clarification.disabled;
+  function createQuestionMark() {
+    var questionMark = document.createElement('span');
+    questionMark.innerHTML = '?';
+    questionMark.className = 'toggleTip';
+    
+    return questionMark;
   }
-          
+  
+  
+  function Single(options) {
+    this._options = options;
+    this._ul;
+  }
+  
+  Single.prototype.getElem = function() {
+    if(!this._ul) this.createList();
+    return this._ul;
+  }
+  
+  Single.prototype.createList = function() {
+    this._ul = document.createElement('ul');
+    for (var i = 0; i < this._options.length; i++) {
+      var li = document.createElement('li');
+      this._ul.appendChild(li);
+      var label = document.createElement('label');
+      label.innerHTML = this._options[i].text;
+      li.appendChild(label);
+      var input = document.createElement('input');
+      input.type = 'radio';
+      input.name = i;
+      label.appendChild(input);
+    }
+  }
+  
+  function Multiple(options) {
+    this._options = options;
+    this._ul;
+  }
+  
+  Multiple.prototype.getElem = function() {
+    if(!this._ul) this.createList();
+    return this._ul;
+  }
+  
+  Multiple.prototype.createList = function() {
+    this._ul = document.createElement('ul');
+    for (var i = 0; i < this._options.length; i++) {
+      var li = document.createElement('li');
+      this._ul.appendChild(li);
+      var label = document.createElement('label');
+      label.innerHTML = this._options[i].text;
+      li.appendChild(label);
+      var input = document.createElement('input');
+      input.type = 'checkbox';
+      label.appendChild(input);
+    }
+  }
