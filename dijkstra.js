51aacf64ae765d2640fb12131609acaf41b66ba4
Venzel13 <Venzel13@gmail.com>
2017-04-28 14:25:20 +0300

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -1,4 +1,4 @@
-var matrix = [ // adjacency matrix
+  var matrix = [ // adjacency matrix
     [0,        10,       14,       Infinity, Infinity, Infinity],
     [Infinity, 0,        3,        Infinity, Infinity, Infinity],
     [Infinity, 3,        0,        12,       17,       Infinity],
@@ -20,24 +20,39 @@ var matrix = [ // adjacency matrix
       }
       vertices[start].value = 0;
     }
+    createVertices();
     
-    
-    function chooseMinVertice() {
-      if(!vertices[i].visited) {
-        
-        if(vertices[i].value < minElement) {
-          minElement = vertices[i].value;
-          indexOfMin = i; 
+    while(true) {
+      var minElement = Infinity;
+      var indexOfMin = -1;
+      
+      function chooseMinVertice() {
+        if(!vertices[i].visited) {
+          if(vertices[i].value < minElement) {
+            minElement = vertices[i].value;
+            indexOfMin = i; 
+          }
         }
       }
-    }
-    
-    function relableVertice() {
-      var newLength = vertices[indexOfMin].value + matrix[indexOfMin][i];
-            
+      for (var i = 0; i < vertices.length; i++) {
+        chooseMinVertice();
+      }
+      
+      if (indexOfMin == -1) break;  // interrupt the infinite circle in case all vertices have been visited
+      
+      function relableVertice() {
+        var newLength = vertices[indexOfMin].value + matrix[indexOfMin][i];
         if (newLength < vertices[i].value) { 
           vertices[i].value = newLength;
         }
+      }
+      
+      for (var i = 0; i < matrix[indexOfMin].length; i++) {
+        if(matrix[indexOfMin][i] !== Infinity || matrix[indexOfMin][i] !== 0) {
+          relableVertice();
+        }
+      }
+      vertices[indexOfMin].visited = true;
     }
     
     function showResult() {
@@ -47,27 +62,7 @@ var matrix = [ // adjacency matrix
       }
       alert(result);
     }
-    
-    createVertices();
-    
-    while(true) {
-      var minElement = Infinity;
-      var indexOfMin = -1;
-      
-      for (var i = 0; i < vertices.length; i++) {
-        chooseMinVertice();
-      }
-      
-      if (indexOfMin == -1) break;  // interrupt the infinite circle in case all vertices have been visited
-
-      for (var i = 0; i < matrix[indexOfMin].length; i++) {
-        if(matrix[indexOfMin][i] !== Infinity || matrix[indexOfMin][i] !== 0) {
-          relableVertice();
-        }
-      }
-      vertices[indexOfMin].visited = true;
-    }
     showResult();
   }
-  
   findDijkstra(matrix);
+  
