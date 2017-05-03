3215102e847e648e505b3fd01c69e25a12d341a0
Venzel13 <Venzel13@gmail.com>
2017-05-03 14:54:09 +0300

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -22,48 +22,45 @@
       return vertices;
     }
     
-    var vertices = createVertices(0);
+    var vertices = createVertices(2);
     
-    while(true) {
-      function chooseMinVertice(list) {
-        var minElement = Infinity;
-        var indexOfMin = -1;
-        
-        for (var i = 0; i < list.length; i++) {
-          if(!list[i].visited) {
-            if(list[i].value < minElement) {
-              minElement = list[i].value;
-              indexOfMin = i; 
-            }
-          }
+    
+    function chooseMinVertice(list) {
+      var minElement = Infinity;
+      var indexOfMin = -1;
           
+      for (var i = 0; i < list.length; i++) {
+        if(!list[i].visited) {
+          if(list[i].value < minElement) {
+            minElement = list[i].value;
+            indexOfMin = i; 
+          }
         }
-        return indexOfMin;
       }
-      
+      return indexOfMin;
+    }
+       
+    while(true) {    
       var indexOfMin = chooseMinVertice(vertices);
-      console.log(indexOfMin)
-      if (indexOfMin == -1) break;
       
+      if (indexOfMin == -1) break;
+  
       function relableVertice(currentPos) {
         for (var i = 0; i < matrix[currentPos].length; i++) {
-          var minEdge = matrix[currentPos][i];
-          
-          if(minEdge !== Infinity || minEdge !== 0) {
-            var newLength = vertices[currentPos].value + minEdge;
-            
-            if (newLength < vertices[i].value) {
-              vertices[i].value = newLength;
-            }
+          var edge = matrix[currentPos][i];
+          var newLength = vertices[currentPos].value + edge;
+              
+          if (newLength < vertices[i].value) {
+            vertices[i].value = newLength;
           }
         }
         return currentPos;
       }
+      
       indexOfMin = relableVertice(indexOfMin);
-        
       vertices[indexOfMin].visited = true;
     }
-    
+
     function showResult() {
       var result = [];
       for (var i = 0; i < vertices.length; i++) {
