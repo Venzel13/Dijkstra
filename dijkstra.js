449ded3521a9ced045911e141dff4c223fcdd3b4
Venzel13 <Venzel13@gmail.com>
2017-05-04 13:08:22 +0300

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -7,67 +7,65 @@
     [Infinity, Infinity, Infinity, 9,        Infinity, 0]
   ];
   
-  var initialVertice = +prompt('Начальная вершина', 0);
-  
   function findDijkstra(matrix) {
-    function createVertices(initialVertice) {
-      var vertices = [];
-      for (var i = 0; i < matrix.length; i++) {
-        vertices.push({
-          value: Infinity,
-          visited: false,
-        });
-      }
-      vertices[initialVertice].value = 0;
-      
-      return vertices;
-    }
-    
+    initialVertice = +prompt('Enter an initial vertice', 0);
     var vertices = createVertices(initialVertice);
-    
-    function chooseMinVertice(vertices) {
-      var minElement = Infinity;
-      var indexOfMin = -1;
-      for (var i = 0; i < vertices.length; i++) {
-        
-        if(!vertices[i].visited) {
-          if(vertices[i].value < minElement) {
-            minElement = vertices[i].value;
-            indexOfMin = i; 
-          }
-        }
-      }
-      return indexOfMin;
-    }
-    
-    function relableVertice() {
-      for (var i = 0; i < matrix[indexOfMin].length; i++) {
-        var edgeValue = matrix[indexOfMin][i];
-        var newVerticeValue = vertices[indexOfMin].value + edgeValue;
-              
-        if (newVerticeValue < vertices[i].value) {
-          vertices[i].value = newVerticeValue;
-        }
-      }
-    }
     var indexOfMin = initialVertice;
     
     while(indexOfMin != -1) {
-      relableVertice();
+      relableVertice(vertices, indexOfMin);
       vertices[indexOfMin].visited = true;
       indexOfMin = chooseMinVertice(vertices);
     }
-
-    function showResult() {
-      var result = [];
-      for (var i = 0; i < vertices.length; i++) {
-        result.push(vertices[i].value);
-      }
-      alert(result);
-    }
-    
-    showResult();
+    showResult(vertices);
   }
   
   findDijkstra(matrix);
+  
+  function relableVertice(vertices, indexOfMin) {
+    for (var i = 0; i < matrix[indexOfMin].length; i++) {
+      var edgeValue = matrix[indexOfMin][i];
+      var newVerticeValue = vertices[indexOfMin].value + edgeValue;
+              
+      if (newVerticeValue < vertices[i].value) {
+        vertices[i].value = newVerticeValue;
+      }
+    }
+  }
+  
+  function chooseMinVertice(vertices) {
+    var minElement = Infinity;
+    var indexOfMin = -1;
+    for (var i = 0; i < vertices.length; i++) {
+        
+      if(!vertices[i].visited) {
+        if(vertices[i].value < minElement) {
+          minElement = vertices[i].value;
+          indexOfMin = i; 
+        }
+      }
+    }
+    return indexOfMin;
+  }
+  
+  function createVertices(initialVertice) {
+    var vertices = [];
+    for (var i = 0; i < matrix.length; i++) {
+      vertices.push({
+        value: Infinity,
+        visited: false,
+      });
+    }
+    vertices[initialVertice].value = 0;
+      
+    return vertices;
+  }
+  
+  function showResult(vertices) {
+    var result = [];
+    for (var i = 0; i < vertices.length; i++) {
+      result.push(vertices[i].value);
+    }
+    alert(result);
+  }
   
