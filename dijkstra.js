920c4b76105eb2bd138fcec072556343ea4d336e
Venzel13 <Venzel13@gmail.com>
2017-05-15 10:32:19 +0300

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -1,4 +1,4 @@
-  var matrix = [ // adjacency matrix
+ var matrix = [ // adjacency matrix
     [0,        10,       14,       Infinity, Infinity, Infinity],
     [Infinity, 0,        3,        Infinity, Infinity, Infinity],
     [Infinity, 3,        0,        12,       17,       Infinity],
@@ -7,37 +7,40 @@
     [Infinity, Infinity, Infinity, 9,        Infinity, 0]
   ];
   
+  var vertices;
+  
   function findDijkstra(matrix, initialVertice) {
-    var vertices = createVertices(initialVertice);
+    vertices = createVertices(initialVertice, matrix);
     var indexOfMin = initialVertice;
     
     while(indexOfMin != -1) {
-      relableVertice(vertices, indexOfMin);
+      relableVertice(vertices, indexOfMin, matrix, initialVertice);
       vertices[indexOfMin].visited = true;
       indexOfMin = chooseMinVertice(vertices);
     }
-    showResult(vertices);
   }
   
   var initialVertice = +prompt('Enter an initial vertice', 0);
   findDijkstra(matrix, initialVertice);
+  showResult(vertices);
   
-  function createVertices(initialVertice) {
+  function createVertices(initialVertice, matrix) {
     var vertices = [];
-    for (var i = 0; i < matrix.length; i++) { //зависимость от наружной матрицы нужно убрать, какой смысл тогда передавать ее как параметр в функцию findDijkstra?
+    for (var i = 0; i < matrix.length; i++) {
       vertices.push({
         value: Infinity,
         visited: false,
       });
     }
-    vertices[initialVertice].value = 0; //логически, это не относится к созданию вершин, это уже шаг самого алгоритма поиска
-      
     return vertices;
   }
   
-  function relableVertice(vertices, indexOfMin) {
-    for (var i = 0; i < matrix[indexOfMin].length; i++) { //matrix[indexOfMin] придумай этой штуке имя:) и убери зависимость от внешней матрицы
-      var edgeValue = matrix[indexOfMin][i];
+  function relableVertice(vertices, indexOfMin, matrix, initialVertice) {
+    vertices[initialVertice].value = 0;
+    var minVerticeEdges = matrix[indexOfMin];
+    
+    for (var i = 0; i < minVerticeEdges.length; i++) {
+      var edgeValue = minVerticeEdges[i];
       var newVerticeValue = vertices[indexOfMin].value + edgeValue;
               
       if (newVerticeValue < vertices[i].value) {
@@ -51,21 +54,18 @@
     var indexOfMin = -1;
     for (var i = 0; i < vertices.length; i++) {
         
-      if(!vertices[i].visited) { //зачем двойной if?
-        if(vertices[i].value < minElement) {
-          minElement = vertices[i].value;
-          indexOfMin = i; 
-        }
+      if(!vertices[i].visited && vertices[i].value < minElement) {
+        minElement = vertices[i].value;
+        indexOfMin = i; 
       }
     }
     return indexOfMin;
   }
   
-  function showResult(vertices) { //по сути ввод данных и показ результата не относятся к самому алгоритму поиска. Вынеси их отдельно.
+  function showResult(vertices) {
     var result = [];
     for (var i = 0; i < vertices.length; i++) {
       result.push(vertices[i].value);
     }
     alert(result);
   }
-  
